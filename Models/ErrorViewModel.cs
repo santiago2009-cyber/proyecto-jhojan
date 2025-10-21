39516f16281e8c2d16b8f8dc615b820585f557ca
santiago2009-cibernético <santiagotarras@gmail.com>
2025-10-21 09:01:48 -0500

    proyecto base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace proyecto_santiago.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
